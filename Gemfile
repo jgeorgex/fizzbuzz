dc34a29d7505fd4f7f929bd8752a5081bed5d621
Julian George <jules104@gmail.com>
2019-11-03 17:50:00 +0000

    Refactor fizzbuzz method remove unecesssary returns and use zero method

---
+++ Gemfile
@@ -0,0 +1,17 @@
+require 'fizzbuzz'
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'rake'
+gem 'rubocop', '0.71.0'
+
+group :test do
+  gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    return "fizzbuzz"
-  elsif number % 3 == 0
-    return "fizz"
-  elsif number % 5 == 0
-    return "buzz"
+  if (number % 3).zero? && (number % 5).zero?
+    'fizzbuzz'
+  elsif (number % 3).zero?
+    'fizz'
+  elsif (number % 5).zero?
+    'buzz'
   else
-    return number
+    number
   end
 end
